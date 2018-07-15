517c2b96d8e7807bb5650900518a8c15a596f55a
Nick Smith <smthnck@gmail.com>
2018-07-15 01:08:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,21 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
 
-function addElementToBeginningOfArray() {
-  
+function addElementToBeginningOfArray(a,e) {
+  var n = [e,...a]
+  return n
 }
 
-function destructivelyAddElementToBeginningOfArray() {
-  
+function destructivelyAddElementToBeginningOfArray(a,e) {
+  a.unshift(e)
+  return a
+}
+
+function addElementToEndOfArray(a,e) {
+  var n = [...a,e]
+  return n
+}
+
+function destructivelyAddElementToEndOfArray(a,e) {
+  a.push(e)
+  return a
 }
